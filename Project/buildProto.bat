c8615b7b90b7a44b6f579ab7b1395cb9d1c278b6
MirMuhammad Jabbari <smjabarimc7@gmail.com>
2023-06-08 13:18:04 +0330

    update.

--- Project/buildProto.bat
+++ Project/buildProto.bat
@@ -1,1 +1,3 @@
-..\Scripts\python -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. ServerBody.proto
+python -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. ServerBody.proto
+
+pause
